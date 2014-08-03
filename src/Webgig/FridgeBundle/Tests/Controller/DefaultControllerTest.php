60de1f6ba3032f331da5a75be903343130fe4aee
Sagar Bhandari <webgig.sagar@gmail.com>
2014-08-03 22:56:16 +1000

    Added form data submission  test

--- src/Webgig/FridgeBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Webgig/FridgeBundle/Tests/Controller/DefaultControllerTest.php
@@ -45,4 +45,49 @@ class DefaultControllerTest extends TypeTestCase
 
         $this->assertTrue($form->isValid());
     }
+
+    public function testValidDataSubmission()
+    {
+           $formData = array(
+            'fridge_csv' => 'bread,10,slices,25/12/2014
+            cheese,10,slices,25/12/2014
+            butter,250,grams,25/12/2014
+            peanut butter,250,grams,2/12/2014
+            mixed salad,150,grams,26/12/2013',
+                        'recipe_json' => '
+            [
+                {
+                    "name": "grilled cheese on toast",
+                    "ingredients": [
+                        { "item":"bread", "amount":"2", "unit":"slices"},
+                        { "item":"cheese", "amount":"2", "unit":"slices"}
+                    ]
+                }
+                ,
+                {
+                    "name": "salad sandwich",
+                    "ingredients": [
+                        { "item":"bread", "amount":"2", "unit":"slices"},
+                        { "item":"mixed salad", "amount":"100", "unit":"grams"}
+                    ]
+                }
+            ]',
+        );
+
+
+        $type = new Fridge();
+        $form = $this->factory->create($type);
+
+
+        $form->submit($formData);
+
+        $this->assertTrue($form->isSynchronized());
+
+        $view = $form->createView();
+        $children = $view->children;
+
+        foreach (array_keys($formData) as $key) {
+            $this->assertArrayHasKey($key, $children);
+        }
+    }
 }
